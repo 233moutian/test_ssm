72882c191ed2c4c3a4be6922a2ba5730b574e165
233moutian <1107022342@qq.com>
2016-08-03 15:59:43 +0800

    半完成品类,用于生成Excel表,未测试

---
+++ src/main/java/aode/ssm/util/Excel.java
@@ -0,0 +1,63 @@
+package aode.ssm.util;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ${周欣文} on 2016/8/3.
+ */
+public class Excel {
+    public void export(){
+        HSSFWorkbook wb = new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet("这里写表名");       // 创建表单并设置其表名
+        List<?> list = new ArrayList<Object>();     // 数据库提取数据
+        HSSFRow tRow = sheet.createRow(0);      // 创建表单行
+        tRow.createCell(0).setCellValue("学号");  // 行头
+            // 这里写行头
+
+
+        for (int i = 1; i <= list.size(); i++) {
+            HSSFRow tRows = sheet.createRow(i);
+            // Checkin checkin = checkinList.get(i - 1);  得到list里面的对象
+//            if (checkin.getS_id() != null) {
+//                DecimalFormat df = new DecimalFormat("0");
+//                String S_id = df.format(checkin.getS_id());
+//                rows.createCell(0).setCellValue(S_id);
+//            }
+//            if (checkin.getName() != null) {
+//                rows.createCell(1).setCellValue(checkin.getName());
+//            }
+
+            //赋值  有几个行写几个
+        }
+        FileOutputStream out = null;
+        String path = "";   // 这里写生成的文件存放路径  跟上传图片的路径类似
+        File file = new File("文件名" + ".xlsx");
+        // 以下是输出文件
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        try {
+            file.createNewFile();
+            out = new FileOutputStream(file);
+            wb.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
