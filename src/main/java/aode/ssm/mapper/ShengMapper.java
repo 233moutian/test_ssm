108d30d04a82609281297592a439824c0922b5a6
233moutian <1107022342@qq.com>
2016-08-11 23:00:19 +0800

    准备三级联动下拉框的效果  修改之前的缺漏  写控制层的类

--- src/main/java/aode/ssm/mapper/ShengMapper.java
+++ src/main/java/aode/ssm/mapper/ShengMapper.java
@@ -1,11 +1,13 @@
 package aode.ssm.mapper;
 
-import aode.ssm.model.Post;
+import aode.ssm.model.Sheng;
 import aode.ssm.util.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by ${周欣文} on 2016/8/5.
  */
-public interface ShengMapper extends BaseMapper<Post> {
+@Repository
+public interface ShengMapper extends BaseMapper<Sheng> {
 
 }
--- src/main/java/aode/ssm/mapper/ShiMapper.java
+++ src/main/java/aode/ssm/mapper/ShiMapper.java
@@ -1,11 +1,13 @@
 package aode.ssm.mapper;
 
-import aode.ssm.model.Post;
+import aode.ssm.model.Shi;
 import aode.ssm.util.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by ${周欣文} on 2016/8/5.
  */
-public interface ShiMapper extends BaseMapper<Post> {
+@Repository
+public interface ShiMapper extends BaseMapper<Shi> {
 
 }
